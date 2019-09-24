419e28ca56a747e733cfba8b9b46225d721e6c97
dingdz <ddzyan@163.com>
2019-09-24 14:58:38 +0800

    修复BUG

--- routers/role.js
+++ routers/role.js
@@ -3,11 +3,11 @@ const express = require("express");
 const RoleController = require("../controllers/role");
 const router = express.Router();
 // 添加角色
-router.post("/manage/role/add", RoleController.add);
+router.post("/add", RoleController.add);
 
 // 获取角色列表
-router.get("/manage/role/list", RoleController.list);
+router.get("/list", RoleController.list);
 
 // 更新角色(设置权限)
-router.post("/manage/role/update", RoleController.update);
+router.post("/update", RoleController.update);
 module.exports = router;
